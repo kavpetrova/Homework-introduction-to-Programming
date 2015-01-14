aa3c608e436833490c7e48e486084cf53cd3e7cc
kavpetrova <kavpetrova@icloud.com>
2015-01-14 22:37:46 +0200

    Introduction to Programing
    My first program

---
+++ AgeAfter10Years/AgeAfter.cs
@@ -0,0 +1,12 @@
+﻿//Problem 15.* Age after 10 Years
+
+//Write a program to read your birthday from the console and print how old you are now and how old you will be after 10 years.
+using System;
+
+    class AgeAfter
+    {
+        static void Main()
+        {
+        }
+    }
+
---
+++ PrintYourName/YourName.cs
@@ -0,0 +1,15 @@
+﻿//Problem 5. Print Your Name
+
+//Modify the previous application to print your name.
+//Ensure you have named the application well (e.g. “PrintMyName”).
+using System;
+
+
+    class YourName
+    {
+        static void Main()
+        {
+            Console.WriteLine("My Name :)");
+        }
+    }
+
